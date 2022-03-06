f0d21a770951d095f9003e40e5f311673c0f7113
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-03-06 17:39:36 -0600

    configurando unit test - intersectionObserver

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,27 @@
+/* eslint-disable no-useless-constructor */
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+
 import '@testing-library/jest-dom';
+
+global.IntersectionObserver = class IntersectionObserver {
+  constructor() { }
+
+  disconnect() {
+    return null;
+  }
+
+  observe() {
+    return null;
+  }
+
+  takeRecords() {
+    return null;
+  }
+
+  unobserve() {
+    return null;
+  }
+};
