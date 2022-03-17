6e833386d5e07a185b683848e0d2ce28b3fc2be9
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-03-17 12:33:25 -0600

    useTitle - hook para cambiar documtn.title dinamicamente para el SEO

---
+++ src/hooks/useTitle.js
@@ -0,0 +1,13 @@
+import { useEffect, useRef } from 'react';
+
+export const useTitle = (title) => {
+
+  const prevTitle = useRef(document.title);
+
+  useEffect(() => {
+    const previousTitle = prevTitle.current;
+    document.title = `Giffy | ${title}`;
+
+    return () => document.title = previousTitle;
+  }, [title]);
+};
--- src/pages/gif/GifDetail.js
+++ src/pages/gif/GifDetail.js
@@ -1,11 +1,15 @@
 import Spinner from 'components/spinner/Spinner';
 import { useGifDetail } from 'hooks/useGifDetail';
+import { useTitle } from 'hooks/useTitle';
 import { Redirect } from 'wouter';
 import GifItem from '../../components/gif/GifItem';
 
 const GifDetail = (props) => {
   const { params } = props;
   const { gif, loading, isError } = useGifDetail(params.id);
+
+  const title = gif ? gif.title : '';
+  useTitle(title);
 
   if (loading) {
     return <Spinner />;
