ed016e07cdf9e761820812e0c931598f9e3421be
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-03-21 14:27:26 -0600

    actualizando ruta de error page

--- src/constants/appRouterConstants.js
+++ src/constants/appRouterConstants.js
@@ -1,7 +1,7 @@
 export const homeR = '/';
 export const gifSearchBR = '/search';
 export const gifDetailBR = '/gif';
-export const error = '/404';
+export const error = '/:rest*';
 
 export const gifSearchR = `${gifSearchBR}/:keyword/:rating?`;
 export const gifDetailR = `${gifDetailBR}/:id`;
