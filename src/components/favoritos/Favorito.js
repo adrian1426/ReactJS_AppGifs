eb0f8c4de5ed725cafc119bb03d489c88425731d
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-03-22 16:10:01 -0600

    comprobando login en click favorito

--- src/components/favoritos/Favorito.js
+++ src/components/favoritos/Favorito.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useLocation } from 'wouter';
+import { login } from 'constants/appRouterConstants';
+import { useUser } from '../../hooks/userHook/useUser';
 
 const Favorito = (props) => {
   const { id } = props;
+  const { isLogged } = useUser();
+  const navigate = useLocation()[1];
 
   const handleClick = () => {
+    if (!isLogged) {
+      return navigate(login);
+    }
+
     alert(id)
   };
 
