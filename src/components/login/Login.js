a06bfc04a2062d5acf328f8acc0e2ec3dbecff74
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-03-25 20:51:28 -0600

    estilizando login component

---
+++ src/components/login/Login.js
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'wouter';
+import { useUser } from '../../hooks/userHook/useUser';
+import './Login.css';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useLocation()[1];
+  const { login, isLogged, loading, error } = useUser();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    login({ username, password });
+  };
+
+  useEffect(() => {
+    if (isLogged) {
+      navigate('/');
+    }
+  }, [isLogged, navigate]);
+
+  if (loading) {
+    return <h1>Cargando...</h1>;
+  }
+
+  if (error) {
+    return <h1>Error login...</h1>;
+  }
+
+  return (
+    <form
+      className='form'
+      onSubmit={handleSubmit}
+    >
+      <label>
+        usuario
+        <input
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+          placeholder='username'
+        />
+      </label>
+
+      <label>
+        password
+        <input
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          type='password'
+          placeholder='password'
+        />
+      </label>
+      <button className='btn'>Login</button>
+    </form>
+  );
+};
+
+export default Login;
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,47 +1,12 @@
-import { useEffect, useState } from 'react';
-import { useLocation } from 'wouter';
-import { useUser } from '../../hooks/userHook/useUser';
+import LoginComponent from "components/login/Login";
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useLocation()[1];
-  const { login, isLogged, loading, error } = useUser();
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    login({ username, password });
-  };
-
-  useEffect(() => {
-    if (isLogged) {
-      navigate('/');
-    }
-  }, [isLogged, navigate]);
-
-  if (loading) {
-    return <h1>Cargando...</h1>;
-  }
-
-  if (error) {
-    return <h1>Error login...</h1>;
-  }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        value={username}
-        onChange={e => setUsername(e.target.value)}
-        placeholder='username'
-      />
-      <input
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-        type='password'
-        placeholder='password'
-      />
-      <button>Login</button>
-    </form>
+    <>
+      <h1>Inicio de Sesión</h1>
+      <LoginComponent />
+    </>
   );
 };
 
