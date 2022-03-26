aae73ad3d5f5c3c230fca381c1e094c69920da3c
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-03-26 16:31:19 -0600

    isSubmitting de Formik

--- src/components/register/RegisterForm.js
+++ src/components/register/RegisterForm.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import { Formik } from 'formik';
+
+const initialValues = {
+  username: '',
+  password: ''
+};
 
 const RegisterForm = () => {
   return (
-    <div>
-      registro de usuarios
-    </div>
+    <>
+      <h2>registro de usuarios</h2>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values) => {
+          console.log(values)
+        }}
+      >
+        {
+          ({
+            handleSubmit,
+            handleChange,
+            isSubmitting
+          }) =>
+          (
+            <form onSubmit={handleSubmit}>
+              <input
+                name='username'
+                onChange={handleChange}
+              />
+              <input
+                name='password'
+                onChange={handleChange}
+              />
+              <button
+                type='submit'
+                className='btn'
+                disabled={isSubmitting}
+              >
+                Registrar {isSubmitting.toString()}
+              </button>
+            </form>
+          )
+        }
+      </Formik>
+    </>
   );
 };
 
