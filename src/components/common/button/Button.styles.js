7b000d2785a9aee63ec036c67f0f25d426e258ea
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-03-30 10:47:59 -0600

    reutilizar estilos comunes - common

--- src/components/common/button/Button.styles.js
+++ src/components/common/button/Button.styles.js
@@ -1,20 +1,22 @@
 import styled from '@emotion/styled';
 import { Link } from 'wouter';
 
-export const LinkStyled = styled(Link)`
-  border: 1px solid transparent;
-  padding: .5rem 1rem;
-  background-color: var(--brand-color_3);
-  color: var(--theme-body-txt);
-  cursor: pointer;
-  font-size: 1rem;
+const common = `
+border: 1px solid transparent;
+padding: .5rem 1rem;
+background-color: var(--brand-color_3);
+color: var(--theme-body-txt);
+cursor: pointer;
+font-size: 1rem;
 
-  :hover{
-    background-color: var(--brand-color_6);
-  }
+:hover{
+  background-color: var(--brand-color_6);
+}
 
-  [disabled]{
-    opacity: .3;
-    pointer-events: none;
-  }
+[disabled]{
+  opacity: .3;
+  pointer-events: none;
+}
 `;
+
+export const LinkStyled = styled(Link)(common);
