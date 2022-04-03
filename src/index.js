c9829efb46e41bc53993b3291a89c2f14367af57
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-04-02 22:28:52 -0600

    theming

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@emotion/react';
+
+const theme = {
+  colors: {
+    primary: 'greeen'
+  }
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
